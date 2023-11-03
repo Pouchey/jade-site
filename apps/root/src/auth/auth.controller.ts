ee4138e7a8b6db1e6f308895a56b067957eb9d91
trouchet <theophile.rouchet@serli.com>
2023-11-03 16:08:53 +0100

    generate tokens

--- apps/root/src/auth/auth.controller.ts
+++ apps/root/src/auth/auth.controller.ts
@@ -1,24 +1,25 @@
 import { ITokens } from '@jaderowley/shared/src/auth/types';
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('auth/login')
-  async login(): Promise<ITokens> {
-    return await this.authService.login();
+  login(@Body() dto: AuthDto): Promise<ITokens> {
+    return this.authService.login(dto);
   }
 
   @Post('auth/logout')
-  async logout() {
-    return await this.authService.logout();
+  logout() {
+    return this.authService.logout();
   }
 
   @Post('auth/refresh')
-  async refresh() {
-    return await this.authService.refresh();
+  refresh() {
+    return this.authService.refresh();
   }
 }
---
+++ apps/root/src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.REFRESH_TOKEN_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: object) {
+    const refreshToken = req.headers.authorization
+      .replace('Bearer ', '')
+      .trim();
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
