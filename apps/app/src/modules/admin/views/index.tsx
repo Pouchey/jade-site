8313a181a120ea4e379978dcee58f22519ad496c
trouchet <theophile.rouchet@serli.com>
2023-11-03 11:02:06 +0100

    auth only for admin

--- apps/app/src/modules/admin/views/index.tsx
+++ apps/app/src/modules/admin/views/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+import { useAuthContext } from '_modules/auth/hooks/useContext';
+import { useFetchMe } from '_modules/me/hooks/useServices';
 
 const Admin = React.memo(() => {
+  useFetchMe();
+
+  const { state } = useAuthContext();
+  const { pathname } = useLocation();
+
+  if (!state?.isLogged)
+    return <Navigate to="/login" state={{ from: pathname }} />;
+
   return <div>Admin</div>;
 });
 
