b1d45097f723bfb60058ceb59cf54a2a8316e424
pouchey <theophilerouchet@gmail.com>
2024-03-01 10:57:30 +0100

    app: fix  env deploy

--- apps/app/src/vite-env.d.ts
+++ apps/app/src/vite-env.d.ts
@@ -3,6 +3,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_API_URL: string;
   readonly VITE_API_MODE: string;
+  readonly VITE_STRIPE_KEY: string;
+  readonly MODE: string;
 }
 
 interface ImportMeta {
