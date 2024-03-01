a9fb63f08a159c1f8b58a050af6c95f41375811c
pouchey <theophilerouchet@gmail.com>
2024-03-01 12:26:26 +0100

    app: fix buttonId stripe

--- apps/app/src/modules/donate/views/index.tsx
+++ apps/app/src/modules/donate/views/index.tsx
@@ -5,10 +5,13 @@ import NotFoundPage from '_components/not-found';
 
 import { StyledWrapper } from './style';
 
+const buttonId = import.meta.env.VITE_STRIPE_BUTTON_ID;
 const key = import.meta.env.VITE_STRIPE_KEY;
 const mode = import.meta.env.MODE;
 
 const Donate = React.memo(() => {
+  if (!key || !buttonId) console.error('Stripe key or button id is not set');
+
   if (!key) return <NotFoundPage />;
 
   if (mode === 'development') return <Label content="Stripe key is not set" />;
@@ -16,10 +19,7 @@ const Donate = React.memo(() => {
   return (
     <StyledWrapper>
       {/* @ts-expect-error - StripeBuyButton is not a known element */}
-      <stripe-buy-button
-        buy-button-id="buy_btn_1OEXBbLQzG1Vs6XzcExZ2a28"
-        publishable-key={key}
-      />
+      <stripe-buy-button buy-button-id={buttonId} publishable-key={key} />
     </StyledWrapper>
   );
 });
--- apps/app/src/vite-env.d.ts
+++ apps/app/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_API_MODE: string;
   readonly VITE_STRIPE_KEY: string;
+  readonly VITE_STRIPE_BUTTON_ID: string;
   readonly MODE: string;
 }
 
