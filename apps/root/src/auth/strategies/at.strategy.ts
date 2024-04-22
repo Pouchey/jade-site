d2f0e999f17fca25b3da19790a0940fa5ddee766
pouchey <theophilerouchet@gmail.com>
2024-04-22 12:52:17 +0200

    root: update packages

--- apps/root/src/auth/strategies/at.strategy.ts
+++ apps/root/src/auth/strategies/at.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 
 import { TJWTPayload } from '../types/jwt';
 
